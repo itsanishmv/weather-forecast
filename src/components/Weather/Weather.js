0ea189d0438fdd2bbb7874b5cdbcc9c3fcf99fbd
Anish M V <anishmv@kofukuidealabs.com>
2024-02-22 04:54:27 +0530

    feature- get browser location and fetch weather data

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -1,9 +1,27 @@
 import React, { useEffect, useState } from "react";
 import useGetWeather from "./hooks/useGetWeather";
-const Weather = () => {
-  const { weatherInfo } = useGetWeather();
 
-  console.log(weatherInfo);
+const Weather = () => {
+  const [currentBrowserLatLong, setCurrentBrowserLatLong] = useState();
+  const { weatherInfo } = useGetWeather(currentBrowserLatLong);
+  useEffect(() => {
+    function getLocation() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+      } else {
+        alert("Geolocation is not supported by this browser.");
+      }
+    }
+
+    function showPosition(position) {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+
+      setCurrentBrowserLatLong({ lat: latitude, long: longitude });
+    }
+    getLocation();
+  }, []);
+  console.log(currentBrowserLatLong);
   return (
     <div className=" border-2 border-black">{weatherInfo?.location?.name}</div>
   );
--- src/components/Weather/hooks/useGetWeather.js
+++ src/components/Weather/hooks/useGetWeather.js
@@ -1,17 +1,23 @@
 import React, { useEffect, useState } from "react";
 import { apiBaseUrl, apiKey } from "../../../constants";
-function useGetWeather() {
+function useGetWeather(currentBrowserLatLong) {
   const [weatherInfo, setWeatherInfo] = useState();
   useEffect(() => {
     async function getWeatherinfo() {
-      const promise = await fetch(
-        `${apiBaseUrl}/v1/current.json?q=11.9716965%2C75.422819%20&key=${apiKey}`
-      );
-      const response = await promise.json();
-      setWeatherInfo(response);
+      try {
+        if (currentBrowserLatLong.lat !== undefined) {
+          const promise = await fetch(
+            `${apiBaseUrl}/v1/current.json?q=${currentBrowserLatLong?.lat}%2C${currentBrowserLatLong?.long}%20&key=${apiKey}`
+          );
+          const response = await promise.json();
+          setWeatherInfo(response);
+        }
+      } catch (error) {
+        console.log(`error ${error}`);
+      }
     }
     getWeatherinfo();
-  }, []);
+  }, [currentBrowserLatLong?.lat]);
 
   return { weatherInfo };
 }
